cb4c1bbbea07e951c0944587faf7e2b722f51f47
Raiden-Refuerzo <79947126+Raiden-Refeuferzo@users.noreply.github.com>
2021-11-16 16:50:35 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 Hps="";
 Sts="";
+song1_status="";
+song2_status="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 function preload()
 {
-    song=loadSound("HP theme.mp3");
-    song=loadSound("Star Wars.mp3");
+    Hps=loadSound("HP theme.mp3");
+    Sts=loadSound("Star Wars.mp3");
 }
 function setup()
 {
@@ -11,9 +17,34 @@ function setup()
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function gotPoses( results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        
+    }
+}
+function modelLoaded()
+{
+    console.log("poseNet is initialized");
 }
 function draw() 
 {
     image(video,0,0,400,300);
-
+    song1_status=Hps.isPlaying();
+    song2_status=Sts.isPlaying();
+}
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
